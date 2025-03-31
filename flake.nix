c61acc9563213727db1e0fb3478bc48a1912b858
winston <hey@winston.sh>
2025-03-31 13:25:21 +0200

    refactor: make it an overlay

--- flake.nix
+++ flake.nix
@@ -6,8 +6,73 @@
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
+    inputs@{ self, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      flake.overlays.default =
+        final: prev:
+        let
+          inherit (final.stdenv.targetPlatform.rust) cargoShortTarget;
+          pinnedWasmtime = inputs.nixpkgs-wasmtime.legacyPackages.${final.stdenv.system}.wasmtime;
+        in
+        {
+          neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (prevAttrs: {
+            buildInputs = prevAttrs.buildInputs ++ [
+              final.wasmtime-29_0_1
+            ];
+            cmakeFlags = prevAttrs.cmakeFlags ++ [
+              (final.lib.cmakeBool "ENABLE_WASMTIME" true)
+            ];
+          });
+
+          tree-sitter = prev.tree-sitter.overrideAttrs (prevAttrs: {
+            buildInputs = (prevAttrs.buildInputs or [ ]) ++ [ final.wasmtime-29_0_1 ];
+            nativeBuildInputs = (prevAttrs.nativeBuildInputs or [ ]) ++ [ final.cmake ];
+
+            cmakeDir = "../lib";
+            cmakeFlags = [
+              (final.lib.cmakeBool "TREE_SITTER_FEATURE_WASM" true)
+              "-DCMAKE_INSTALL_INCLUDEDIR=include"
+              "-DCMAKE_INSTALL_LIBDIR=lib"
+            ];
+            cargoBuildFeatures = [ "wasm" ];
+
+            configurePhase = "cmakeConfigurePhase && cd ..";
+            postBuild = "cmake --build $cmakeBuildDir";
+
+            postInstall = ''
+              cmake --install $cmakeBuildDir
+              mv $out/share/pkgconfig $out/lib/pkgconfig
+              installShellCompletion --cmd tree-sitter \
+                --bash <("$out/bin/tree-sitter" complete --shell bash) \
+                --zsh <("$out/bin/tree-sitter" complete --shell zsh) \
+                --fish <("$out/bin/tree-sitter" complete --shell fish)
+            '';
+          });
+
+          wasmtime-29_0_1 = pinnedWasmtime.overrideAttrs {
+            postInstall =
+              ''
+                # move libs from out to dev
+                install -d -m 0755 $dev/lib
+                install -m 0644 ''${!outputLib}/lib/* $dev/lib
+                rm -r ''${!outputLib}/lib
+
+                install -d -m0755 $dev/include/wasmtime
+                # https://github.com/rust-lang/cargo/issues/9661
+                install -m0644 \
+                  target/${cargoShortTarget}/release/build/wasmtime-c-api-impl-*/out/include/*.h \
+                  $dev/include
+                install -m0644 \
+                  target/${cargoShortTarget}/release/build/wasmtime-c-api-impl-*/out/include/wasmtime/*.h \
+                  $dev/include/wasmtime
+              ''
+              + final.lib.optionalString final.stdenv.hostPlatform.isDarwin ''
+                install_name_tool -id \
+                  $dev/lib/libwasmtime.dylib \
+                  $dev/lib/libwasmtime.dylib
+              '';
+          };
+        };
       systems = [
         "aarch64-darwin"
         "aarch64-linux"
@@ -15,80 +80,12 @@
         "x86_64-linux"
       ];
       perSystem =
-        {
-          inputs',
-          pkgs,
-          system,
-          ...
-        }:
-        let
-          pinnedWasmtime = inputs'.nixpkgs-wasmtime.legacyPackages.wasmtime;
-        in
+        { pkgs, system, ... }:
         {
           _module.args.pkgs = import inputs.nixpkgs {
             inherit system;
-            overlays = [
-              (final: prev: {
-                neovim-unwrapped =
-                  (prev.neovim-unwrapped.overrideAttrs (prevAttrs: {
-                    buildInputs = prevAttrs.buildInputs ++ [
-                      final.wasmtime-29_0_1
-                    ];
-                    cmakeFlags = prevAttrs.cmakeFlags ++ [
-                      (final.lib.cmakeBool "ENABLE_WASMTIME" true)
-                    ];
-                  })).override
-                    {
-                      tree-sitter = final.tree-sitter-with-wasm;
-                    };
-
-                tree-sitter-with-wasm = prev.tree-sitter.overrideAttrs (prevAttrs: {
-                  buildInputs = (prevAttrs.buildInputs or [ ]) ++ [ final.wasmtime-29_0_1 ];
-                  nativeBuildInputs = (prevAttrs.nativeBuildInputs or [ ]) ++ [ final.cmake ];
-
-                  cmakeDir = "../lib";
-                  cmakeFlags = [
-                    (final.lib.cmakeBool "TREE_SITTER_FEATURE_WASM" true)
-                    "-DCMAKE_INSTALL_INCLUDEDIR=include"
-                    "-DCMAKE_INSTALL_LIBDIR=lib"
-                  ];
-                  cargoBuildFeatures = [ "wasm" ];
-
-                  configurePhase = "cmakeConfigurePhase && cd ..";
-                  postBuild = "cmake --build $cmakeBuildDir";
-
-                  postInstall = ''
-                    cmake --install $cmakeBuildDir
-                    mv $out/share/pkgconfig $out/lib/pkgconfig
-                    installShellCompletion --cmd tree-sitter \
-                      --bash <("$out/bin/tree-sitter" complete --shell bash) \
-                      --zsh <("$out/bin/tree-sitter" complete --shell zsh) \
-                      --fish <("$out/bin/tree-sitter" complete --shell fish)
-                  '';
-                });
-
-                wasmtime-29_0_1 = pinnedWasmtime.overrideAttrs {
-                  postInstall =
-                    ''
-                      # move libs from out to dev
-                      install -d -m 0755 $dev/lib
-                      install -m 0644 ''${!outputLib}/lib/* $dev/lib
-                      rm -r ''${!outputLib}/lib
-
-                      install -d -m0755 $dev/include/wasmtime
-                      # https://github.com/rust-lang/cargo/issues/9661
-                      install -m0644 target/${final.stdenv.targetPlatform.rust.cargoShortTarget}/release/build/wasmtime-c-api-impl-*/out/include/*.h $dev/include
-                      install -m0644 target/${final.stdenv.targetPlatform.rust.cargoShortTarget}/release/build/wasmtime-c-api-impl-*/out/include/wasmtime/*.h $dev/include/wasmtime
-                    ''
-                    + final.lib.optionalString final.stdenv.hostPlatform.isDarwin ''
-                      install_name_tool -id \
-                        $dev/lib/libwasmtime.dylib \
-                        $dev/lib/libwasmtime.dylib
-                    '';
-                };
-              })
-            ];
             config = { };
+            overlays = [ self.overlays.default ];
           };
 
           devShells.default = pkgs.mkShell {
@@ -100,7 +97,6 @@
               rust-bindgen
               rustc
               rustfmt
-              wasmtime-29_0_1
             ];
           };
 
@@ -109,9 +105,8 @@
               neovim
               neovim-unwrapped
               tree-sitter
-              tree-sitter-with-wasm
-              wasmtime-29_0_1
               wasmtime
+              wasmtime-29_0_1
               ;
           };
         };
